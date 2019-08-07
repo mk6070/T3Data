db54847e2dd804d42059f56b081b6528d61b57a4
mk6070 <39609983+mk6070@users.noreply.github.com>
2019-08-07 14:05:15 -0400

    Add files via upload

---
+++ T3datasets.R
@@ -0,0 +1,12 @@
+PhenotypeTrials_wheat <- read_csv("~/Downloads/PhenotypeTrials_wheat.csv")
+PhenotypeData_wheat <- read_csv("~/Downloads/PhenotypeData_wheat.csv")
+GeneticCharacters_wheat <- read_csv("~/Downloads/GeneticCharacters_wheat.csv")
+LineRecords_wheat <- read_csv("~/Downloads/LineRecords_wheat.csv")
+
+
+GC_Wheat_melt <- melt(GeneticCharacters_wheat, id=c("Nae", "Property"))
+GC_Wheat_Spread <- dcast(GC_Wheat_melt, Nae ~ Property + variable, value.var = "value")
+
+PhenotypeData_melt <- melt(PhenotypeData_wheat, id=c("Trial", "Trait"))
+PhenotypeData_Spread <- dcast(PhenotypeData_melt, Trial ~ Trait + variable, value.var="value")
+
